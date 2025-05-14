f79990e967a54820a343b81e27f128fb7d4e840a
lul_prof <israelmutua539@gmail.com>
2025-05-14 17:50:21 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ from app import create_app, socketio
 
 app = create_app()
 
+# Initialize database tables
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
     
     socketio.run()  
